50d8992e46c9b643e44bb34bd6e2fc3a05621351
pingvinix <154405821+pingvinix@users.noreply.github.com>
2023-12-24 08:33:41 +0300

    Update firefox_install.sh

--- asset/firefox_install.sh
+++ asset/firefox_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/121.0/linux-x86_64/ru/firefox-121.0.tar.bz2 --trust-server-names=on --restrict-file-names=nocontrol 2>&1 | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Скорость \2\/s, осталось \3/' | zenity --progress --percentage=0 --title="Загрузка файла" --auto-close
+wget -O /tmp/firefox-esr_115.5.sb --no-check-certificate 'https://docs.google.com/uc?export=download&id=1TJH9hP9oUT0hhrzr9k8jfGxuQ-kqd0VL' 2>&1 | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Скорость \2\/s, осталось \3/' | zenity --progress --width 300 --percentage=0 --title="Загрузка файла" --auto-close
