7d9440e4ca79a0915dcb246f00101f851d3c172c
pingvinix <154405821+pingvinix@users.noreply.github.com>
2023-12-25 11:55:30 +0300

    Create anydesk_install.sh

---
+++ asset/anydesk_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+wget -O /tmp/firefox-esr_115.5.sb --no-check-certificate 'https://docs.google.com/uc?export=download&id=1TJH9hP9oUT0hhrzr9k8jfGxuQ-kqd0VL' 2>&1 | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Скорость \2\/s, осталось \3/' | zenity --progress --width 300 --percentage=0 --title="Загрузка файла" --auto-close
+sudo mv /tmp/firefox-esr_115.5.sb /run/initramfs/memory/data/linux/modules/firefox-esr_115.5.sb
