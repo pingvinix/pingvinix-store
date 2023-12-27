0180dd45e61d0079fcf925756a502c932af47832
pingvinix <154405821+pingvinix@users.noreply.github.com>
2023-12-27 10:04:58 +0500

    Update anydesk_install.sh

--- asset/anydesk_install.sh
+++ asset/anydesk_install.sh
@@ -3,3 +3,4 @@
 
 wget -O /tmp/anydesk_6.3.0.sb --no-check-certificate 'https://docs.google.com/uc?export=download&id=1TJlgVQEGmwJeNzTpeSlSoH27IaZ6vcph' 2>&1 | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Скорость \2\/s, осталось \3/' | zenity --progress --width 300 --percentage=0 --title="Загрузка файла" --auto-close
 sudo mv /tmp/anydesk_6.3.0.sb /run/initramfs/memory/data/linux/modules/anydesk_6.3.0.sb
+/tmp/store/asset/anydesk.sh
