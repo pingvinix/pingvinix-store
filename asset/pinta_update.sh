a002a966342d0ab95fff35c0fdc10ed97e967400
pingvinix <154405821+pingvinix@users.noreply.github.com>
2024-01-03 22:24:14 +0500

    Update pinta_update.sh

--- asset/pinta_update.sh
+++ asset/pinta_update.sh
@@ -2,4 +2,4 @@
 
 sudo rm /run/initramfs/memory/data/linux/modules/pinta* | zenity --progress --title='Ждите' --pulsate --auto-close --auto-kill --text="Идет обновление"
 killall xlunch
-/tmp/store/asset/pinta.sh
+/tmp/store/asset/pinta_install.sh
