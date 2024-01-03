34c23191e53a41ff0bca5e6730a2eea45dd89039
pingvinix <154405821+pingvinix@users.noreply.github.com>
2024-01-03 22:24:35 +0500

    Update supertux_update.sh

--- asset/supertux_update.sh
+++ asset/supertux_update.sh
@@ -2,4 +2,4 @@
 
 sudo rm /run/initramfs/memory/data/linux/modules/supertux* | zenity --progress --title='Ждите' --pulsate --auto-close --auto-kill --text="Идет обновление"
 killall xlunch
-/tmp/store/asset/supertux.sh
+/tmp/store/asset/supertux_install.sh
