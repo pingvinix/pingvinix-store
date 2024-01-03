00557bc3e0e31b145e1367b83480b6e9266176c3
pingvinix <154405821+pingvinix@users.noreply.github.com>
2024-01-03 22:19:34 +0500

    Update supertux_install.sh

--- asset/supertux_install.sh
+++ asset/supertux_install.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+wget -O /tmp/supertux_0.6.3.sb --no-check-certificate 'https://docs.google.com/uc?export=download&id=1OD9t5bDuSLJ2gHll4Y7mB7j3d8HlwIIF' 2>&1 | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Скорость \2\/s, осталось \3/' | zenity --progress --width 300 --percentage=0 --title="Загрузка файла" --auto-close
+sudo mv /tmp/supertux_0.6.3.sb /run/initramfs/memory/data/linux/modules/supertux_0.6.3.sb
+/tmp/store/asset/supertux.sh
