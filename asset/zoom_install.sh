20ca50568bc68ca36b662e28ddca2f4d7e791d99
pingvinix <154405821+pingvinix@users.noreply.github.com>
2024-01-04 01:28:43 +0500

    Update zoom_install.sh

--- asset/zoom_install.sh
+++ asset/zoom_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -O /tmp/zoom_5.16.10.sb --no-check-certificate 'https://docs.google.com/uc?export=download&id=1TJlgVQEGmwJeNzTpeSlSoH27IaZ6vcph' 2>&1 | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Скорость \2\/s, осталось \3/' | zenity --progress --width 300 --percentage=0 --title="Загрузка файла" --auto-close
+wget -O /tmp/zoom_5.16.10.sb --no-check-certificate 'https://docs.google.com/uc?export=download&id=13loJx6wO7nDqVLUgx6J_ENeC_l1cDszF' 2>&1 | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Скорость \2\/s, осталось \3/' | zenity --progress --width 300 --percentage=0 --title="Загрузка файла" --auto-close
 sudo mv /tmp/zoom_5.16.10.sb /run/initramfs/memory/data/linux/modules/zoom_5.16.10.sb
 /tmp/store/asset/zoom.sh
