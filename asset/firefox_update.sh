d2a57c1ba20e776ceeda56e8e443f97e97c9ede5
pingvinix <154405821+pingvinix@users.noreply.github.com>
2024-01-03 22:22:12 +0500

    Update firefox_update.sh

--- asset/firefox_update.sh
+++ asset/firefox_update.sh
@@ -2,4 +2,4 @@
 
 sudo rm /run/initramfs/memory/data/linux/modules/firefox-esr* | zenity --progress --title='Ждите' --pulsate --auto-close --auto-kill --text="Идет обновление"
 killall xlunch
-/tmp/store/asset/firefox.sh
+/tmp/store/asset/firefox_install.sh
