ae22621e6d6659029ac304f4746602eaf0967f0d
pingvinix <154405821+pingvinix@users.noreply.github.com>
2024-01-03 22:10:01 +0500

    Update anydesk_delete.sh

--- asset/anydesk_delete.sh
+++ asset/anydesk_delete.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+sudo rm /run/initramfs/memory/data/linux/modules/anydesk* | zenity --progress --title='Ждите' --pulsate --auto-close --auto-kill --text="Идет удаление"
+killall xlunch
+/tmp/store/asset/anydesk.sh
