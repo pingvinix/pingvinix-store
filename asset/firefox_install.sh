48c1807d6c91370c931807485f0e6248afc197fb
pingvinix <154405821+pingvinix@users.noreply.github.com>
2024-01-05 01:53:14 +0500

    Update firefox_install.sh

--- asset/firefox_install.sh
+++ asset/firefox_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-wget -O /tmp/firefox-esr_115.5.sb --no-check-certificate 'https://docs.google.com/uc?export=download&id=1TJH9hP9oUT0hhrzr9k8jfGxuQ-kqd0VL' 2>&1 | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Скорость \2\/s, осталось \3/' | zenity --progress --width 300 --percentage=0 --title="Загрузка файла" --auto-close
-sudo mv /tmp/firefox-esr_115.5.sb /run/initramfs/memory/data/linux/modules/firefox-esr_115.5.sb
+wget -O /tmp/firefox-esr_115.6.sb --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ZdVXQlfGJJSup39dCAOIJuRLfNXNaisW' 2>&1 | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Скорость \2\/s, осталось \3/' | zenity --progress --width 300 --percentage=0 --title="Ждите" --auto-close
+sudo mv /tmp/firefox-esr_115.6.sb /run/initramfs/memory/data/linux/modules/firefox-esr_115.6.sb
 /tmp/store/asset/firefox.sh
