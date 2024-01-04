39c112eb04651d0f96fb0786d451dff5645f0973
pingvinix <154405821+pingvinix@users.noreply.github.com>
2024-01-04 15:20:08 +0500

    Update libreoffice_install.sh

--- asset/libreoffice_install.sh
+++ asset/libreoffice_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
-wget -O /tmp/libreoffice_7.5.9.2.sb --no-check-certificate 'https://docs.google.com/uc?export=download&id=1kaxt0llnXQJP669ZgsIixQwo-sB_J_92' 2>&1 | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Скорость \2\/s, осталось \3/' | zenity --progress --width 300 --percentage=0 --title="Загрузка файла" --auto-close
+rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1kaxt0llnXQJP669ZgsIixQwo-sB_J_92' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1kaxt0llnXQJP669ZgsIixQwo-sB_J_92" -O /tmp/libreoffice_7.5.9.2.sb 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \(.*\)/\1\n# Идет загрузка \1/' | zenity --progress --width 300 --percentage=0 --title="Ждите" --auto-close
+rm -rf /tmp/cookies.txt
 sudo mv /tmp/libreoffice_7.5.9.2.sb /run/initramfs/memory/data/linux/modules/libreoffice_7.5.9.2.sb
-/tmp/store/asset/libreoffice.sh
+/tmp/store/asset/supertux.sh
